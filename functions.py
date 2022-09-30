322c61d850646daf7173d24fd12f5ccb3dcadcfe
DanielMwandiki <danmasketcha@gmail.com>
2022-09-30 13:46:24 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+#funtion to print hello world
+def hello_word():
+    print ('Hello World')
+#call function
+hello_word()
+
+# return a funtion
+def hello_world():
+    return 'Hello Word'
+#assign a variablr
+greeting=hello_world()
+print(greeting)
