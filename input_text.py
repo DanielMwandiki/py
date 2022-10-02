4594e065336ba407dcf3e27a1e3f9834281dff1e
DanielMwandiki <danmasketcha@gmail.com>
2022-10-02 11:15:42 +0300

    input from a text file

---
+++ input_text.py
@@ -0,0 +1,13 @@
+from cgitb import text
+
+
+def open_input(file):
+    with open(file, 'r') as f:
+        text = f.read() #We use read() to read the actual contents of the file
+        print(text)
+
+def main():
+    open_input("text.txt")
+
+if __name__=="__main__":
+    main()
