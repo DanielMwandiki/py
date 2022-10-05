5cf367c6db625f63d6f478c243bf615d6809e2f1
DanielMwandiki <danmasketcha@gmail.com>
2022-10-05 09:36:02 +0300

    looping a json file

---
+++ simple_loop.py
@@ -0,0 +1,14 @@
+fruit = ['apples', 'oranges', 'bananas']
+for item in fruit:
+    print(f'The best fruit now is {item}')
+
+for number in range(100):
+    print(f'The next number is {number}')
+
+for numbers in range(1,10):
+    print(f'The next number is {numbers}')
+
+#odd numbers
+
+for items in range(1,10,2):
+    print(f'The next number is {items}')
